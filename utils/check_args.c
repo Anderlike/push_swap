073b1608204bd83b564064f2bc2fcc06e757704a
Pio <77726736+Anderlike@users.noreply.github.com>
2025-02-17 17:50:08 +0000

    Fixed more leaks.

--- utils/check_args.c
+++ utils/check_args.c
@@ -6,7 +6,7 @@
 /*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:58:07 by aaleixo-          #+#    #+#             */
-/*   Updated: 2025/02/17 17:16:36 by aaleixo-         ###   ########.fr       */
+/*   Updated: 2025/02/17 17:48:42 by aaleixo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,31 +46,47 @@ static int	ft_isnum(char *num)
 	return (1);
 }
 
+char	**get_args(int argc, char **argv, int *is_allocated)
+{
+	char	**args;
+
+	if (argc == 2)
+	{
+		args = ft_split(argv[1], ' ');
+		*is_allocated = 1;
+		if (!args)
+			ft_error();
+	}
+	else
+	{
+		args = argv;
+		*is_allocated = 0;
+	}
+	return (args);
+}
+
 void	ft_check_args(int argc, char **argv)
 {
 	int		i;
 	char	**args;
+	int		is_allocated;
 
-	i = 0;
-	if (argc == 2)
-	{
-		args = ft_split(argv[1], ' ');
-	}
+	args = get_args(argc, argv, &is_allocated);
+	if (is_allocated)
+		i = 0;
 	else
-	{
 		i = 1;
-		args = argv;
-	}
 	while (args[i])
 	{
 		if (!ft_isnum(args[i]) || ft_contains(ft_atoi(args[i]), args, i)
-			|| ((ft_atoi(args[i]) < INT_MIN || ft_atoi(args[i]) > INT_MAX)))
+			|| (ft_atoi(args[i]) < INT_MIN || ft_atoi(args[i]) > INT_MAX))
 		{
-			ft_free(args);
+			if (is_allocated)
+				ft_free(args);
 			ft_error();
 		}
 		i++;
 	}
-	if (argc == 2)
+	if (is_allocated)
 		ft_free(args);
 }
